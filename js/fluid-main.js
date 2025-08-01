39780cf40f10f2b5454c387a7855a23050100430
Dom Taylor <70032243+dominicjtaylor@users.noreply.github.com>
2025-08-01 23:05:23 +0100

    Create fluid-main.js

---
+++ js/fluid-main.js
@@ -0,0 +1,7 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
+import { gsap } from 'https://cdn.skypack.dev/gsap@3.12.2';
+import { ScrollTrigger } from 'https://cdn.skypack.dev/gsap@3.12.2/ScrollTrigger.js';
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Core setup (as in previous message)...
